b8b9df0f982b079d623a9bbdbbafb782347e1624
Zy-jay <93726670+Zy-jay@users.noreply.github.com>
2021-11-25 11:47:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,197 @@
+const serverUrl = "https://20r4o4fubkgu.usemoralis.com:2053/server"; //Server url from moralis.io
+const appId = "AJtSlSTh2okU29lwFxZUz2NQckgmtR80FZIdD7Xl"; // Application id from moralis.io
+
+let currentTrade = {};
+let currentSelectSide;
+let tokens;
+
+async function init() {
+  await Moralis.start({ serverUrl, appId });
+  await Moralis.enableWeb3();
+  await listAvailableTokens();
+  currentUser = Moralis.User.current();
+  if (currentUser) {
+    document.getElementById("swap_button").disabled = false;
+  }
+}
+
+async function listAvailableTokens() {
+  const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+    chain: "polygon", // The blockchain you want to use (eth/bsc/polygon)
+  });
+  tokens = result.tokens;
+  let parent = document.getElementById("token_list");
+  for (const address in tokens) {
+    let token = tokens[address];
+    let div = document.createElement("div");
+    div.setAttribute("data-address", address);
+    div.className = "token_row";
+    let html = `
+        <img class="token_list_img" src="${token.logoURI}">
+        <span class="token_list_text">${token.symbol}</span>
+        `;
+    div.innerHTML = html;
+    div.onclick = () => {
+      selectToken(address);
+    };
+    parent.appendChild(div);
+  }
+}
+
+function selectToken(address) {
+  closeModal();
+  console.log(tokens);
+  currentTrade[currentSelectSide] = tokens[address];
+  console.log(currentTrade);
+  renderInterface();
+  getQuote();
+}
+
+function renderInterface() {
+  if (currentTrade.from) {
+    document.getElementById("from_token_img").src = currentTrade.from.logoURI;
+    document.getElementById("from_token_text").innerHTML = currentTrade.from.symbol;
+  }
+  if (currentTrade.to) {
+    document.getElementById("to_token_img").src = currentTrade.to.logoURI;
+    document.getElementById("to_token_text").innerHTML = currentTrade.to.symbol;
+  }
+}
+
+
+
+function openModal(side) {
+  currentSelectSide = side;
+  document.getElementById("token_modal").style.display = "block";
+}
+function closeModal() {
+  document.getElementById("token_modal").style.display = "none";
+}
+
+async function getQuote() {
+  if (!currentTrade.from || !currentTrade.to || !document.getElementById("from_amount").value) return;
+
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
+
+  const quote = await Moralis.Plugins.oneInch.quote({
+    chain: "polygon", // The blockchain you want to use (eth/bsc/polygon)
+    fromTokenAddress: currentTrade.from.address, // The token you want to swap
+    toTokenAddress: currentTrade.to.address, // The token you want to receive
+    amount: amount,
+  });
+  console.log(quote);
+  document.getElementById("gas_estimate").innerHTML = quote.estimatedGas;
+  document.getElementById("to_amount").value = quote.toTokenAmount / 10 ** quote.toToken.decimals;
+}
+
+async function trySwap() {
+  let address = Moralis.User.current().get("ethAddress");
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
+  if (currentTrade.from.symbol !== "MATIC") {
+    const allowance = await Moralis.Plugins.oneInch.hasAllowance({
+      chain: "polygon", // The blockchain you want to use (eth/bsc/polygon)
+      fromTokenAddress: currentTrade.from.address, // The token you want to swap
+      fromAddress: address, // Your wallet address
+      amount: amount,
+    });
+    console.log(allowance);
+    if (!allowance) {
+      await Moralis.Plugins.oneInch.approve({
+        chain: "polygon", // The blockchain you want to use (eth/bsc/polygon)
+        tokenAddress: currentTrade.from.address, // The token you want to swap
+        fromAddress: address, // Your wallet address
+      });
+    }
+  }
+  try {
+    let receipt = await doSwap(address, amount);
+    alert("Swap Complete");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function doSwap(userAddress, amount) {
+  return Moralis.Plugins.oneInch.swap({
+    chain: "polygon", // The blockchain you want to use (eth/bsc/polygon)
+    fromTokenAddress: currentTrade.from.address, // The token you want to swap
+    toTokenAddress: currentTrade.to.address, // The token you want to receive
+    amount: amount,
+    fromAddress: userAddress, // Your wallet address
+    slippage: 1,
+  });
+}
+
+init();
+
+document.getElementById("modal_close").onclick = closeModal;
+document.getElementById("from_token_select").onclick = () => {
+  openModal("from");
+};
+document.getElementById("to_token_select").onclick = () => {
+  openModal("to");
+};
+document.getElementById("from_amount").onblur = getQuote;
+document.getElementById("swap_button").onclick = trySwap;
+
+
+async function login() {
+  let user = await Moralis.authenticate()
+  document.getElementById("btn-login").innerHTML = user.get("ethAddress");
+  document.getElementById("btn-logout").disabled = false;
+  document.getElementById("btn-login").onclick = copyAddress;  
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+
+  try {
+    currentUser = Moralis.User.current();
+    if (!currentUser) {
+       currentUser = await Moralis.authenticate();
+    }
+    document.getElementById("swap_button").disabled = false;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+ async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+    document.getElementById("btn-login").onclick = login;
+    document.getElementById("btn-login").innerHTML = ("Login");
+    document.getElementById("swap_button").disabled = true;
+    document.getElementById("btn-logout").disabled = true;
+  }
+
+  async function copyAddress() {
+
+      var str = document.getElementById("btn-login").innerText;
+      // Create new element
+      var el = document.createElement('textarea');
+      // Set value (string to be copied)
+      el.value = str;
+      // Set non-editable to avoid focus and move outside of view
+      el.setAttribute('readonly', '');
+      el.style = {position: 'absolute', left: '-9999px'};
+      document.body.appendChild(el);
+      // Select text inside element
+      el.select();
+      // Copy text to clipboard
+      document.execCommand('copy');
+      // Remove temporary element
+      document.body.removeChild(el);
+
+      }
+
+  
+  document.getElementById("btn-login").onclick = login;
+  document.getElementById("btn-logout").onclick = logOut;
+
+
+
+
+
+
+  
